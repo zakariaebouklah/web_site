6b7931e5a64d87cb443a4d0ec7bbb562456f0892
zZ7_aki <zakariae.bouklah18@gmail.com>
2022-09-28 16:16:47 +0100

    More Styling

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,6 +37,8 @@ class RegistrationController extends AbstractController
                 )
             );
 
+            $user->setLastSeen(new \DateTimeImmutable());
+
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
